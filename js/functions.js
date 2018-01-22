fb954f0adfe382e7c817584bd1597a7ac904a271
Eric T Cormack <bobryuu@gmail.com>
2018-01-21 22:16:43 -0600

    Still nothing

--- js/functions.js
+++ js/functions.js
@@ -53,7 +53,7 @@ function  workLoad() {
 		newTitle = $this.find('strong').text(),
 		newfolder = $this.find('.thumb-unit').data('folder'),
 		spinner = '<div class="loader">Loading...</div>',
-		newHTML = document.URL + 'work/' + newfolder;
+		newHTML = document.URL + 'work/' + newfolder + '/';
 		console.log(newHTML);
 
 		var element = document.getElementsByClassName('project-load');
@@ -66,7 +66,7 @@ function  workLoad() {
 			});
 		});
 
-		$('.project-load').html(spinner).load(newHTML);
+		//$('.project-load').html(spinner).load(newHTML);
 		$('.project-title').text(newTitle);
 	});
 
